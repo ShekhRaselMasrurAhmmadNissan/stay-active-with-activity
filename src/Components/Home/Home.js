cffeee1bf6a0a1f6074f72e0418025e529bd3ce5
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-09-28 23:48:08 +0600

    Loaded the fake data.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Activities from '../Activities/Activities';
 import Header from '../Header/Header';
 
 const Home = () => {
+	const [activities, setActivities] = useState([]);
+
+	useEffect(() => {
+		const loadActivities = async () => {
+			try {
+				const res = await fetch('fakeData.json');
+				const data = await res.json();
+				setActivities(data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		loadActivities();
+	}, []);
+
 	return (
 		<div>
+			<h1>{activities.length}</h1>
 			<Header />
 			<Activities />
 		</div>
