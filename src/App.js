16beef8c6d6c07e07bf89df0f21d8d23a5d57d72
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-09-28 22:20:42 +0600

    Added and tested React Font Awesome.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookReader } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Stay Active With Activity</h1>
+			<h1>
+				<p>Hello</p>{' '}
+				<FontAwesomeIcon
+					icon={faBookReader}
+					className="w-8 h-8 text-blue-500"
+				></FontAwesomeIcon>
+			</h1>
 		</div>
 	);
 }
