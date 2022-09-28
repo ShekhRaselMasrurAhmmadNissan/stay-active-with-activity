f2451b230e3860631cf098789c5a36586bd82c2b
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-09-28 23:00:01 +0600

    Added Tailwind Themes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#f7807e',
+
+					secondary: '#f4a6de',
+
+					accent: '#ddc749',
+
+					neutral: '#212D36',
+
+					'base-100': '#252955',
+
+					info: '#7BD8EF',
+
+					success: '#5EE4AE',
+
+					warning: '#92730C',
+
+					error: '#EB7085',
+				},
+			},
+		],
 	},
 	plugins: [require('daisyui')],
 };
