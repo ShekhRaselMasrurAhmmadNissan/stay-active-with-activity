73312c64ba316eda61f80fdbd0a3d27cbb9a20c8
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-09-29 21:43:05 +0600

    Final Commit before submit.

--- src/Components/ActivityDetails/ActivityDetails.js
+++ src/Components/ActivityDetails/ActivityDetails.js
@@ -4,12 +4,14 @@ const ActivityDetails = ({ activityTime, breakTime }) => {
 	return (
 		<div className="mt-8 mx-1 py-4 text-black bg-white rounded-md">
 			<h1 className="text-xl font-semibold ml-2">Activity Details</h1>
-			<p className="bg-gray-100 py-2 m-1 px-1 rounded-md text-blue-600">
-				Required Time For Activity: {activityTime}
+			<p className="bg-gray-100 py-2 m-1 px-1 rounded-md text-black">
+				Required Time For Activity:{' '}
+				<h1 className="text-blue-500 inline">{activityTime}</h1>
 				<small className="text-black">minutes</small>
 			</p>
-			<p className="bg-gray-100 py-2 m-1 px-1 rounded-md text-blue-600">
-				Break Time: {breakTime}
+			<p className="bg-gray-100 py-2 m-1 px-1 rounded-md text-black">
+				Break Time:{' '}
+				<h1 className="text-blue-500 inline">{breakTime}</h1>
 				<small className="text-black">minutes</small>
 			</p>
 		</div>
